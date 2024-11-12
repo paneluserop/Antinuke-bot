a4bd29f1c042ed8a8d12c55b53dd71138aafe9e2
paneluserop <abhijeetpanda946@gmail.com>
2024-11-12 15:27:24 +0530

    Update config.js

--- Settings/config.js
+++ Settings/config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     TOKEN: "" || process.env.TOKEN, //Your Bot Token
     MONGO_URI: "" || process.env.MONGO_URI, //Mongo Uri
-    EMBED_COLOR: "" || process.env.EMBED_COLOR, //Embed Color
+    EMBED_COLOR: "#87e6d3" || process.env.EMBED_COLOR, //Embed Color
     DEV_ID: "" || process.env.DEV_ID, //Developer ID
     OWNER_ID: "" || process.env.OWNER_ID, //Owner ID
 
